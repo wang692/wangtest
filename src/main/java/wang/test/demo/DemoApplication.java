4ee57b50250bac06eef5140c2c027957e5751a39
wang692 <2028965152@qq.com>
2022-02-26 13:26:19 +0800

    v4 from cz

--- src/main/java/wang/test/demo/DemoApplication.java
+++ src/main/java/wang/test/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("vim from wzh");
         System.out.println("vim from cz");
+        System.out.println("vim from cz2");
         SpringApplication.run(DemoApplication.class, args);
     }
 
